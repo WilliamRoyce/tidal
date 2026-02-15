2a4e5971e7b63842983647732f091103ddc3995d
William Royce <wr286@cam.ac.uk>
2026-02-15 10:13:04 +0000

    feat: Changed output from .png to .npz + tidal measure --source A_0,A_1,A_2 --target B_0,B_1,B_2

--- examples/coupled_proca/run.sh
+++ examples/coupled_proca/run.sh
@@ -11,4 +11,10 @@ tidal simulate examples/data/coupled_proca_3d.json \
   --param mA2=1.0 --param mB2=2.0 --param gcoup=0.5 \
   --ic gaussian --grid-size 16 --t-end 2.0 --dt 0.05 \
   --bc periodic,periodic --scheme runge-kutta \
-  --output outputs/coupled_proca_output.png
+  --output outputs/coupled_proca_output.npz
+
+# Measure conversion between vector field groups and mixing length
+tidal measure outputs/coupled_proca_output.npz \
+  --spec examples/data/coupled_proca_3d.json \
+  --what conversion,mixing \
+  --source A_0,A_1,A_2 --target B_0,B_1,B_2
